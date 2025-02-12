882d3799d03d601b123e0b997a9fefd555ada7af
luna <lunapadilla.8@gmail.com>
2025-02-12 17:46:29 +0100

    solved lab

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
-
-
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const PORT = 5005;
+const projects = require("./data/projects.json");
+const articles = require("./data/articles.json");
 // CREATE EXPRESS APP
 // Here you should create your Express app:
 
-
+const app = express();
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
@@ -14,12 +17,28 @@
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
 
-
+app.use(express.static("public"));
+app.use(express.json());
+app.use(logger("dev"));
 
 // ROUTES
 // Start defining your routes here:
 
-
-
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+app.get("/blog", (request, response, next) =>
+  response.sendFile(path.join(__dirname , "/views/blog.html"))
+);
+app.get("/api/projects", (request, response, next) => {
+  response.json(projects);
+});
+app.get("/api/articles", (request, response, next) => {
+  response.json(articles);
+});
+app.use((request, response, next) => {
+  response.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+});
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(PORT, () => console.log(`My first app listening on port ${PORT}`));
